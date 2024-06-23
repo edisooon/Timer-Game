6f3c17521d908bb8f5c7a044117a28441a4a94ee
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-06-23 15:34:34 -0500

    implemented name input and submission using state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [name, setName] = useState("");
+  const [prevName, setPrevName] = useState("unknown entity");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    const curName = name;
+    setPrevName(curName);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {prevName}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={name} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
