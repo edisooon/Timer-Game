a3dffad61d5377927467bcf791b96209c453738b
ezbuckeye <105604551+ezbuckeye@users.noreply.github.com>
2024-06-23 16:23:08 -0500

    implemented TimerChallenge component with useRef to clear the timer instance when being stoped

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,17 +1,36 @@
-export default function TimerChallenge({title, targetTime}) {
+import { useState, useRef } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timerRef = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timerRef.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timerRef.current);
+  }
+
   return (
-    <section className = "challenge">
+    <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>Time's up, you lost!</p>}
       <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
+        {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>
-          Start Challenge
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
-      <p className="">
-        Time is running... / Timer is inactive
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer is inactive"}
       </p>
     </section>
   );
